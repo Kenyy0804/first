e4475623399535fccd460be34a834e38723707e9
Kenyy0804 <159415549+Kenyy0804@users.noreply.github.com>
2024-07-01 15:08:10 +0900

    event show

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Event extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'information',
+        'max_people',
+        'start_date',
+        'end_date',
+        'is_visible',
+    ];
+
+    protected function eventDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => Carbon::parse($this->attributes['start_date'])->format('Y年m月d日'),
+        );
+    }
+
+    protected function startTime(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => Carbon::parse($this->attributes['start_date'])->format('H時i分'),
+        );
+    }
+
+    protected function endTime(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => Carbon::parse($this->attributes['end_date'])->format('H時i分'),
+        );
+    }
 }
